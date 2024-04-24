80f1afdcb9e32168080d1df500b3e65763f5905e
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2024-04-24 01:59:11 -0500

    feat: :rocket: components
    files components
    
    Jhon

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit, input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+})
+export class InputComponent  implements OnInit {
+
+
+  @Input() control!: FormControl
+  @Input() type!: string
+  @Input() label!: string
+  @Input() autocomplete!: string
+  @Input() icon!: string
+
+  isPassword!: boolean
+  hide: boolean = true
+
+  constructor() { }
+
+  ngOnInit() {
+    if (this.type == 'password') this.isPassword = true
+  }
+
+  showPassword() {
+    this.hide = !this.hide
+
+    if (this.hide) this.type = 'password'
+    else this.type = 'text'
+  }
+
+}
